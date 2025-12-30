d9f3e09a9d7bf98caa93d656030ad3b1c8afb493
1cbyc <isaacnsisong@gmail.com>
2025-12-30 07:02:30 +0100

    fix(src\server): Update context.ts

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,6 +1,7 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/prisma/client';
 
-const prisma = new PrismaClient();
+
+export const prisma = new PrismaClient({});
 
 export function createContext() {
   return { prisma };
