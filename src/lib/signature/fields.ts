8b62596a588e4b4adf024bdb0de92d036d621dee
1cbyc <isaacnsisong@gmail.com>
2025-12-30 05:32:34 +0100

    feat(src\lib\signature): Add fields.ts

---
+++ src/lib/signature/fields.ts
@@ -0,0 +1,5 @@
+// Placeholder for signature field placement logic
+export function addSignatureField(pdfBuffer: Buffer, fieldName: string) {
+  // TODO: Implement signature field placement
+  return pdfBuffer;
+}
