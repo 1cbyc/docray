6acd58efe628f71f1a67403d9dcc4b04313dbae6
1cbyc <isaacnsisong@gmail.com>
2025-12-30 07:02:27 +0100

    fix(src\lib\signature): Update sign.ts

--- src/lib/signature/sign.ts
+++ src/lib/signature/sign.ts
@@ -1,7 +1,8 @@
-import { plainAddPlaceholder, sign } from 'node-signpdf';
+import { plainAddPlaceholder, SignPdf } from 'node-signpdf';
 import fs from 'fs';
 
 export function signPDF(pdfBuffer: Buffer, p12Buffer: Buffer, passphrase: string) {
-  const pdfWithPlaceholder = plainAddPlaceholder({ pdfBuffer });
-  return sign(pdfWithPlaceholder, p12Buffer, { passphrase });
+  const pdfWithPlaceholder = plainAddPlaceholder({ pdfBuffer, reason: 'Document signed' });
+  const signer = new SignPdf();
+  return signer.sign(pdfWithPlaceholder, p12Buffer, { passphrase });
 }
