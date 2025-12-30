8d073be33e1b63fa3eedec8ce36275c79647f899
1cbyc <isaacnsisong@gmail.com>
2025-12-30 07:02:28 +0100

    fix(src\pages\api\auth): Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,9 +2,7 @@ import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import EmailProvider from 'next-auth/providers/email';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '../../../server/context';
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
