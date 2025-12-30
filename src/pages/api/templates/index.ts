2bf14e43f45c6bb1f9236b5613ab47102c3cb95e
1cbyc <isaacnsisong@gmail.com>
2025-12-30 05:36:10 +0100

    feat(src\pages\api\templates): Add index.ts

---
+++ src/pages/api/templates/index.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    const templates = await prisma.template.findMany({ orderBy: { createdAt: 'desc' } });
+    return res.status(200).json(templates);
+  }
+  res.status(405).end();
+}
