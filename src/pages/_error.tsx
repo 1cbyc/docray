9592369bb5d994d9a121e7afcb37b65dfc055333
1cbyc <isaacnsisong@gmail.com>
2025-12-30 05:32:41 +0100

    feat(src\pages): Add _error.tsx

---
+++ src/pages/_error.tsx
@@ -0,0 +1,17 @@
+import { NextPageContext } from 'next';
+
+function Error({ statusCode }: { statusCode?: number }) {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-4">{statusCode ? `Error ${statusCode}` : 'An error occurred'}</h1>
+      <p>Sorry, something went wrong.</p>
+    </div>
+  );
+}
+
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
