27eaafe5107c099f507e617695648160f5531291
1cbyc <isaacnsisong@gmail.com>
2025-12-30 05:32:39 +0100

    feat(src\pages): Add 404.tsx

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
