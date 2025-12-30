bcc2aa94a0170106150dc380006d8c3468dd9c47
1cbyc <isaacnsisong@gmail.com>
2025-12-30 05:30:16 +0100

    feat(src\pages): Add approvals.tsx

---
+++ src/pages/approvals.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function ApprovalsPage() {
+  return (
+    <>
+      <Head>
+        <title>Approvals | docray</title>
+      </Head>
+      <main className="min-h-screen p-8">
+        <h1 className="text-2xl font-bold mb-4">Approvals</h1>
+        {/* TODO: Approval workflow UI */}
+      </main>
+    </>
+  );
+}
