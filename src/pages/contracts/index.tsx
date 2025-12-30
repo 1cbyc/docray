c1168f37a0fbd5b19faea6806e0349e244766cfa
1cbyc <isaacnsisong@gmail.com>
2025-12-30 05:30:16 +0100

    feat(src\pages\contracts): Add index.tsx

---
+++ src/pages/contracts/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function ContractsPage() {
+  return (
+    <>
+      <Head>
+        <title>Contracts | docray</title>
+      </Head>
+      <main className="min-h-screen p-8">
+        <h1 className="text-2xl font-bold mb-4">Contracts</h1>
+        {/* TODO: List contracts, add links to details */}
+      </main>
+    </>
+  );
+}
