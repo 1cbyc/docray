a8700464155a881a6a35264a0938f280770d4e1f
1cbyc <isaacnsisong@gmail.com>
2025-12-31 14:43:14 +0100

    feat(src\utils): Add trpc.ts

---
+++ src/utils/trpc.ts
@@ -0,0 +1,26 @@
+import { createTRPCNext } from '@trpc/next';
+import { httpBatchLink } from '@trpc/client';
+import type { AppRouter } from '../server/root';
+
+function getBaseUrl() {
+  if (typeof window !== 'undefined') {
+    return '';
+  }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  return `http://localhost:${process.env.PORT ?? 3001}`;
+}
+
+export const trpc = createTRPCNext<AppRouter>({
+  config() {
+    return {
+      links: [
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
+    };
+  },
+  ssr: false,
+});
