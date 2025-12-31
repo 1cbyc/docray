da9f417789ef55c9f2212ae946d580756f30a894
1cbyc <isaacnsisong@gmail.com>
2025-12-31 14:43:10 +0100

    fix(src\server): Update context.ts

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,6 +1,9 @@
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+});
 
 export function createContext() {
   return { prisma };
