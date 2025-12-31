a3b0bf90ad8a5f8dd041901c61522a107d7d5dbc
1cbyc <isaacnsisong@gmail.com>
2025-12-31 14:47:06 +0100

    fix(prisma): Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,17 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
+import { createClient } from '@libsql/client';
 
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
+const libsql = createClient({
+  url: process.env.DATABASE_URL!,
 });
+
+const adapter = new PrismaLibSQL(libsql);
+
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   // Create sample users
