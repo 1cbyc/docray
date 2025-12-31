e37a45f4a654b97362a45756f656ee2030c6a08f
1cbyc <isaacnsisong@gmail.com>
2025-12-31 15:33:12 +0100

    fix(src\server): Update context.ts

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,10 +1,17 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../pages/api/auth/[...nextauth]';
 
 export const prisma = new PrismaClient();
 
-export function createContext() {
-  return { prisma };
+export async function createContext({ req, res }: any) {
+  const session = await getServerSession(req, res, authOptions);
+
+  return {
+    prisma,
+    session,
+  };
 }
 
-export type Context = ReturnType<typeof createContext>;
+export type Context = Awaited<ReturnType<typeof createContext>>;
